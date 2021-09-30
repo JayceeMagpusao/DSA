d2e4e6b53202b3d985f7a9cae0be0711cb54c549
jaycee <jay27mag@yahoo.com>
2021-09-30 15:19:18 -0700

    compress DSA

--- DS&A_Problems.js
+++ DS&A_Problems.js
@@ -48,3 +48,26 @@ const uncompress = (s) => {
   }
   return newString;
 };
+
+// 004 compress
+const compress = (s) => {
+  let result = [];
+  let i = 0;
+  let j = 0;
+  
+  while (j <= s.length) {
+    if (s[i] === s[j]) {
+      j += 1;
+    } else {
+      const num = j - i;
+      if (num === 1) {
+        result.push(s[i]);
+      } else {
+        result.push(String(num), s[i]);
+      }
+      i = j;
+    }
+  }
+  
+  return result.join('');
+};
