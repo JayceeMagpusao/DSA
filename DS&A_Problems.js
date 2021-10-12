32166f5a6bee3a7a192c44d12498eb51cf48d897
jaycee <jay27mag@yahoo.com>
2021-10-12 14:07:17 -0700

    DSA

--- DS&A_Problems.js
+++ DS&A_Problems.js
@@ -234,3 +234,23 @@ ADD Aspect_ratio float
 DROP TABLE Movies
 
 DROP TABLE Boxoffices
+
+const uncompress = (s) => {
+  let result = [];
+  const numbers = '0123456789';
+  let i = 0;
+  let j = 0;
+  while (j < s.length) {
+    if (numbers.includes(s[j])) {
+      j += 1;
+    } else {
+      const num = Number(s.slice(i, j));
+      for (let count = 0; count < num; count += 1) {
+        result.push(s[j]);
+      }
+      j += 1;
+      i = j;
+    }
+  }
+  return result.join('');
+};
